e4bbc391105457f1a9fe251b2cb0f717471c176b
Mona Lisa <had17007@byui.edu>
2019-02-06 15:26:08 -0700

    ...

---
+++ mood.js
@@ -0,0 +1,37 @@
+var entry = {
+  time: "",
+  mood: "",
+  note: "",
+//   entry: function() {
+//     return this.time + "<br><h3>" + this.mood + "</h3>" + this.note + ",<br>";
+// }
+}
+
+function submit() {
+  var mood = document.forms['entry']['mood'].value;
+  var time = document.forms['entry']['time'].value;
+  var note = document.forms['entry']['note'].value;
+
+  entry.time = time;
+  entry.mood = mood;
+  entry.note = note;
+
+  var strEntry = JSON.stringify(entry);
+  localStorage.setItem(time, strEntry);
+
+  var ret = localStorage.getItem(time);
+  var ret1 = JSON.parse(ret);
+  printMood(ret1);
+}
+
+function printMood(thing) {
+    alert();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("moodlist").innerHTML = this.responseText;
+    }
+  }
+  xhttp.open("GET", thing, true);
+  xhttp.send();
+}
