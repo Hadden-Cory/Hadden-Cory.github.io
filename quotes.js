6b0c544c4f94f1ff710bba71bbdf56fd70144a22
Mona Lisa <had17007@byui.edu>
2019-02-16 13:15:42 -0700

    ...

--- quotes.js
+++ quotes.js
@@ -2,10 +2,11 @@ function grabAQuote(){
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("list").innerHTML = this.responseText;
+            var item = JSON.parse(xhttp.response);
+            document.getElementById("list").innerHTML = "\""+item.quote+"\"<br>-"+ item.autor;
           }
         }
         
-        xhttp.open("GET", quotes.JSON, true);
+        xhttp.open("GET", "quotes.JSON", true);
         xhttp.send();
 }
