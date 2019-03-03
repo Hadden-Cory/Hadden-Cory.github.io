37332e8d68d42669d4055c8f283021cd8cf170e4
Mona Lisa <had17007@byui.edu>
2019-03-03 00:01:33 -0700

    Reconfiguring to be more OO

--- mood.js
+++ mood.js
@@ -1,41 +1,87 @@
-var entry = {
-   time: "",
-   date: "",
-   note: "",
-   opt1: "",
-   opt2: ""
-};
+class Entry {
+  constructor(time, date, note) {
+    this.time = time || '';
+    this.date = date || '';
+    this.note = note || "None";
+  }
 
-function submit(form, option0, option1) {
+  getTime(){
+    return time;
+  }
 
- 
+  getDate(){
+    return date;
+  }
 
-  var date = document.forms[form]['date'].value+"";
-  var time = document.forms[form]['time'].value+"";
-  var note = document.forms[form]['note'].value+"";
-  var opt0 = document.forms[form][option0].value+"";
+  getNote(){
+    return note;
+  }
+}
+
+class Weather extends Entry {
+  constructor(time, date, note, weather) {
+    super(time, date, note);
+    this.weather = weather || "Sunny";
+  }
+
+  getWeather(){
+    return weather;
+  }
+}
+
+// function Mood(mood) {
+//   Entry.call(this, time, date, note);
+//   this.mood = mood || "Neutral";
+// }
 
 
-  entry.time = time;
-  entry.date = date;
-  entry.note = note;
-  entry.opt0 = opt0;
-  
-  if (option1!=""){
-    var opt1 = document.forms[form][option1].value+"";
-    entry.opt1 = opt1;
-  };
- 
-  var strEntry = JSON.stringify(entry);
-  localStorage.setItem(time+date, strEntry);
-  printMood();
+// function Exercise(exercise, duration) {
+//   Entry.call(this, time, date, note);
+//   this.exercise = exercise;
+//   this.duration = duration;
+// }
+
+
+function submit(form, type) {
+
+    var date = document.forms[form]['date'].value + "";
+    alert(date);
+    var time = document.forms[form]['time'].value + "";
+    alert(time);
+    var note = document.forms[form]['note'].value + "";
+    alert(note);
+
+  if (type == "weather") {
+    alert("in to weather if");
+
+    var weather = document.forms[form]['weather'].value + "";
+    alert(weather);
+
+    var weath1 = new Weather(time, date, note, weather);
+
+    alert(weath.));
+  }
+
+  // entry.time = time;
+  // entry.date = date;
+  // entry.note = note;
+  // entry.opt0 = opt0;
+
+  // if (option1!=""){
+  //   var opt1 = document.forms[form][option1].value+"";
+  //   entry.opt1 = opt1;
+  // };
+
+  // var strEntry = JSON.stringify(entry);
+  // localStorage.setItem(time+date, strEntry);
+  // printMood();
   // var ret = localStorage.getItem(time);
   // var ret1 = JSON.parse(ret);
 
- 
+
 }
 
-function writeToFile(){
+function writeToFile() {
   var fileWriter = require('fileWriter');
 
 
@@ -45,45 +91,46 @@ function writeToFile(){
 function printMood() {
   var table = document.getElementById('notes');
 
-for (var i = 0; i < localStorage.length; i++){
-  
-  var key = localStorage.key(i);
-  var item = localStorage.getItem(key);
-  
+  for (var i = 0; i < localStorage.length; i++) {
 
-  console.log("Before Parse: "+item);
-  var pitem = JSON.parse(item);
- 
-  console.log("Note after Parse: " + pitem.opt0);
-  var row = table.insertRow(i);
-  var cell0 = row.insertCell(0);
-  var cell1 = row.insertCell(1);
-  var cell2 = row.insertCell(2);
-  var cell3 = row.insertCell(3);
-  cell0.innerHTML = pitem.opt0;
-  cell1.innerHTML = pitem.note;
-  cell2.innerHTML = pitem.time;
-  cell3.innerHTML = pitem.date;
-
-}
-//   var tableRow = document.createElement("TR");
-//   var rowInsertion = document.createTextNode(dataInsertion);
-//   tableRow.appendChild(rowInsertion);
-//   document.getElementById("notes").appendChild(tableData);  
-
-// }var tableData = document.createElement("TD");
-//   var dataInsertion = document.createTextNode(pitem.note);
-//   tableData.appendChild(dataInsertion);
+    var key = localStorage.key(i);
+    var item = localStorage.getItem(key);
 
 
-  
+    console.log("Before Parse: " + item);
+    var pitem = JSON.parse(item);
+
+    console.log("Note after Parse: " + pitem.opt0);
+    var row = table.insertRow(i);
+    var cell0 = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    var cell3 = row.insertCell(3);
+    cell0.innerHTML = pitem.opt0;
+    cell1.innerHTML = pitem.note;
+    cell2.innerHTML = pitem.time;
+    cell3.innerHTML = pitem.date;
+
+  }
+  //   var tableRow = document.createElement("TR");
+  //   var rowInsertion = document.createTextNode(dataInsertion);
+  //   tableRow.appendChild(rowInsertion);
+  //   document.getElementById("notes").appendChild(tableData);  
+
+  // }var tableData = document.createElement("TD");
+  //   var dataInsertion = document.createTextNode(pitem.note);
+  //   tableData.appendChild(dataInsertion);
+
+
+
   // var xhttp = new XMLHttpRequest();
   // xhttp.onreadystatechange = function() {
   //   if (this.readyState == 4 && this.status == 200) {
   //     document.getElementById("moodlist").innerHTML = this.responseText;
   //   }
   // }
-  
-  // xhttp.open("GET", thing, true);
-  // xhttp.send();alert("ajax");
+
+// xhttp.open("GET", thing, true);
+// xhttp.send();alert("ajax");
 }
+
--- quotes.js
+++ quotes.js
@@ -1,17 +1,17 @@
 function grabAQuote(){
-    alert(1);
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            alert(2);
-          if (this.readyState == 4 && this.status == 200) {
-              alert(3);
-            var item = JSON.parse(xhttp.response);
-            alert(item.author);
 
-            //document.getElementById("list").innerHTML = "<li>\""+item.quote+"\"<br>-"+ item.author+"</li>";
-          }
-        }
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function() {
+            
+          if (this.readyState == 4 && this.status == 200) {
+
+            var item = JSON.parse(xhttp.response);
+
+            document.getElementById("list").innerHTML = "<li>\""+item.quote+"\"<br>-"+ item.author+"</li>";
+          };
+        };
         
-       // xhttp.open("GET", "quotes.json", true);
-        //xhttp.send();
+       xhttp.open("GET", "quotes.json", true);
+       xhttp.send();
 }
