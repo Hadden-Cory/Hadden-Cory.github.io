4cf32733d367ddf27fcb9e417d32cc458ab5a391
Mona Lisa <had17007@byui.edu>
2019-03-07 01:12:35 -0700

    Nearly Done

--- mood.js
+++ mood.js
@@ -1,146 +1,308 @@
+
+/***********Objects*Prototypes*************/
 class Entry {
-  constructor(time, date, note) {
-    this.time = time || '';
-    this.date = date || '';
-    this.note = note || "None";
+  constructor(time, date, note, type) {
+    this.time = time || 'time not specified';
+    this.date = date || 'date not specified';
+    this.note = note || "No additional notes";
+    this.type = type || "unknown entryu type";
   }
 
-  getTime(){
-    return time;
+  //setters
+  setTime(time) {
+    this.time = time;
   }
 
-  getDate(){
-    return date;
+  setDate(date) {
+    this.date = date;
   }
 
-  getNote(){
-    return note;
+  setNote(note) {
+    this.note = note;
+  }
+
+  setNote(type) {
+    this.type = type;
+  }
+
+  //getters
+  getTime() {
+    return this.time;
+  }
+
+  getDate() {
+    return this.date;
+  }
+
+  getNote() {
+    return this.note;
+  }
+
+  getType() {
+    return this.type;
   }
 }
 
 class Weather extends Entry {
   constructor(time, date, note, weather) {
-    super(time, date, note);
+    let type = 'weather';
+    super(time, date, note, type);
     this.weather = weather || "Sunny";
   }
 
-  getWeather(){
-    return weather;
+  setWeather(weather) {
+    this.weather = weather;
+  }
+
+  getWeather() {
+    return this.weather;
   }
 }
 
 class Mood extends Entry {
   constructor(time, date, note, mood) {
-    super(time, date, note);
+    let type = 'mood';
+    super(time, date, note, type);
     this.mood = mood || "Neutral";
   }
 
-  getMood(){
-    return mood;
+  setMood(mood) {
+    this.mood = mood;
+  }
+
+  getMood() {
+    return this.mood;
   }
 }
 
 class Exercise extends Entry {
-  constructor(time, date, note, exercise, duration) {
-    super(time, date, note);
-    this.exercise = exercise || "Running";
-    this.duration = duration || "30";
+  constructor(time, date, note, workout, duration) {
+    let type = 'exercise';
+    super(time, date, note, type);
+    this.workout = workout || "Running";
+    this.duration = duration || "10 Minutes";
   }
 
-  getExercise(){
-    return exercise;
+  setWorkout(workout) {
+    this.workout = workout;
   }
 
-  getDuration(){
-    return exercise;
+  setDuration(duration) {
+    this.duration = duration;
+  }
+
+  getWorkout() {
+    return this.workout;
+  }
+
+  getDuration() {
+    return this.duration;
   }
 }
 
-function submit(form, type) {
+/***********Meat*Functions*************/
 
-    var date = document.forms[form]['date'].value + "";
-    alert(date);
-    var time = document.forms[form]['time'].value + "";
-    alert(time);
-    var note = document.forms[form]['note'].value + "";
-    alert(note);
+/*
+FUNCTION SAVE OBJ
+  Receievs form data, builds it into appropriate object, and sends its it off 
+  to be stored in local storage.
+*/
+function saveObj(form, type) {
 
+  // Normal options
+  let date = document.forms[form]['date'].value + "";
+  let time = document.forms[form]['time'].value + "";
+  let note = document.forms[form]['note'].value + "";
+
+  //Form specific data options
   if (type == "weather") {
-    alert("in to weather if");
 
     let weather = document.forms[form]['weather'].value + "";
-    alert(weather);
+    let weatherObj = new Weather(time, date, note, weather);
+    storeObj(weatherObj);
 
-    let weath1 = new Weather(time, date, note, weather);
+  } else if (type == "mood") {
 
-    var strEntry = JSON.stringify(weath1);
+    let mood = document.forms[form]['mood'].value + "";
+    let moodObj = new Mood(time, date, note, mood);
+    storeObj(moodObj);
 
-    alert(strEntry);
+  } else if (type == "exercise") {
+
+    let workout = document.forms[form]['workout'].value + "";
+    let duration = document.forms[form]['duration'].value + "";
+    let exerciseObj = new Exercise(time, date, note, workout, duration);
+    storeObj(exerciseObj);
+
+  } else {
+
+    alert("Submission Error: Please reload the page.")
+
+  }
+  removeList();
+  buildList();
+}
+
+/*
+FUNCTION STORE OBJ
+  Receives an object, stringifies it, and sends it to local storage.
+*/
+function storeObj(obj) {
+  let strEntry = JSON.stringify(obj);
+  localStorage.setItem(obj.type + ', ' + obj.getDate() + ', ' + obj.getTime(), strEntry);
+}
+
+/*
+FUNCTION RETREIVE OBJ
+  Receives an object type and date, pulls it from local storage, parses it into an object,
+  and returns it for use stringifies it, and sends it to local storage.
+*/
+function retreiveObj(type, date, time) {
+
+  let retStr = localStorage.getItem(type + ', ' + date + ', ' + time);
+  let retObj = JSON.parse(retStr);
+  return retObj;
+}
+
+function removeList() {
+  let list = document.getElementById('list');
+
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+}
+
+/*
+FUNCTION BUILD TABLE
+
+*/
+function buildList() {
+
+  let journal = [];
+
+  for (let i = 0; i < localStorage.length; i++) {
+
+    let key = localStorage.key(i);
+    let entry = localStorage.getItem(key);
+    let pEntry = JSON.parse(entry);
+    journal.push(pEntry);
+
   }
 
-  // entry.time = time;
-  // entry.date = date;
-  // entry.note = note;
-  // entry.opt0 = opt0;
+  let sortedJournal = journal.sort(sortByTimeStamp);
+  let date = "";
+  let time = "";
 
-  // if (option1!=""){
-  //   var opt1 = document.forms[form][option1].value+"";
-  //   entry.opt1 = opt1;
-  // };
+  let printHeading = document.createElement("H1");
+  let text = document.createTextNode("Journal");
+  printHeading.appendChild(text);
+  document.getElementById("list").appendChild(printHeading);
 
-  // var strEntry = JSON.stringify(entry);
-  // localStorage.setItem(time+date, strEntry);
-  // printMood();
-  // var ret = localStorage.getItem(time);
-  // var ret1 = JSON.parse(ret);
+  for (let i = 0; i < sortedJournal.length; i++) {
+
+    let rowDAO = sortedJournal[i];
+
+    if (date == rowDAO.date) {
+      //Do nothing
+    } else {
+      printDate = document.createElement("H2");
+      text = document.createTextNode(rowDAO.date);
+      printDate.appendChild(text);
+      document.getElementById("list").appendChild(printDate);
+      date = rowDAO.date;
+    }
+
+    if (time == rowDAO.time) {
+      //Do nothing
+    } else {
+      printTime = document.createElement("H3");
+      text = document.createTextNode(rowDAO.time);
+      printTime.appendChild(text);
+      document.getElementById("list").appendChild(printTime);
+      time = rowDAO.time;
+    }
+
+    let row = "";
+
+    if (rowDAO.type == 'weather') {
+      row += rowDAO.weather + "";
+    } else if (rowDAO.type == 'mood') {
+      row += rowDAO.mood + "";
+    } else if (rowDAO.type == 'exercise') {
+      row += rowDAO.duration + " of " + rowDAO.workout;
+    } else {
+      console.log('Unrecognized Object Type');
+    }
+
+    row += " -- " + rowDAO.note;
+
+    printRow = document.createElement("P");
+    text = document.createTextNode(row);
+    printRow.appendChild(text);
+    document.getElementById("list").appendChild(printRow);
+  }
+
+
+
+
 
 
 }
 
-
-function printMood() {
-  var table = document.getElementById('notes');
-
-  for (var i = 0; i < localStorage.length; i++) {
-
-    var key = localStorage.key(i);
-    var item = localStorage.getItem(key);
-
-
-    console.log("Before Parse: " + item);
-    var pitem = JSON.parse(item);
-
-    console.log("Note after Parse: " + pitem.opt0);
-    var row = table.insertRow(i);
-    var cell0 = row.insertCell(0);
-    var cell1 = row.insertCell(1);
-    var cell2 = row.insertCell(2);
-    var cell3 = row.insertCell(3);
-    cell0.innerHTML = pitem.opt0;
-    cell1.innerHTML = pitem.note;
-    cell2.innerHTML = pitem.time;
-    cell3.innerHTML = pitem.date;
-
+function sortByTimeStamp(a, b) {
+  let comparison = 0;
+  if (a.date >= b.date && a.time > b.time) {
+    comparison = 1;
+  } else if (a.date <= b.date && a.time < b.time) {
+    comparison = -1;
   }
-  //   var tableRow = document.createElement("TR");
-  //   var rowInsertion = document.createTextNode(dataInsertion);
-  //   tableRow.appendChild(rowInsertion);
-  //   document.getElementById("notes").appendChild(tableData);  
-
-  // }var tableData = document.createElement("TD");
-  //   var dataInsertion = document.createTextNode(pitem.note);
-  //   tableData.appendChild(dataInsertion);
-
-
-
-  // var xhttp = new XMLHttpRequest();
-  // xhttp.onreadystatechange = function() {
-  //   if (this.readyState == 4 && this.status == 200) {
-  //     document.getElementById("moodlist").innerHTML = this.responseText;
-  //   }
-  // }
-
-// xhttp.open("GET", thing, true);
-// xhttp.send();alert("ajax");
+  return comparison;
 }
 
+//   let row = table.insertRow(i);
+//   let cell0 = row.insertCell(0);
+//   let cell1 = row.insertCell(1);
+//   let cell2 = row.insertCell(2);
+//   let cell3 = row.insertCell(3);
+//   cell0.innerHTML = pitem.opt0;
+//   cell1.innerHTML = pitem.note;
+//   cell2.innerHTML = pitem.time;
+//   cell3.innerHTML = pitem.date;
+
+// }
+//   let tableRow = document.createElement("TR");
+//   let rowInsertion = document.createTextNode(dataInsertion);
+//   tableRow.appendChild(rowInsertion);
+//   document.getElementById("notes").appendChild(tableData);  
+
+//   let tableData = document.createElement("TD");
+//   let dataInsertion = document.createTextNode(pitem.note);
+//   tableData.appendChild(dataInsertion);
+
+// let sortedWeatherAry = weatherAry.sort(sortByTimeStamp);
+// let sortedMoodAry = moodAry.sort(sortByTimeStamp);
+// let sortedExerciseAry = exerciseAry.sort(sortByTimeStamp);
+
+// let weatherAry = []; 
+// let moodAry = [];
+// let exerciseAry = []; 
+
+// if (pEntry.type == 'weather'){
+//   weatherAry.push(pEntry);
+// } else if (pEntry.type == 'mood'){
+//   moodAry.push(pEntry);
+// } else if (pEntry.type == 'exercise'){
+//   exerciseAry.push(pEntry);
+// } else {
+//   console.log('Unrecognized Object Type');
+// }
+
+// for (let i = 0; i < sortedWeatherAry.length; i++){
+//   console.log(sortedWeatherAry[i]);
+// }
+// for (let i = 0; i < sortedMoodAry.length; i++){
+//   console.log(sortedMoodAry[i]);
+// }
+// for (let i = 0; i < sortedExerciseAry.length; i++){
+//   console.log(sortedExerciseAry[i]);
+// }
