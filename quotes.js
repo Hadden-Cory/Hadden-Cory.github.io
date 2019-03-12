595ddba4080aefbc5a634896f8402bd2fcd6d3df
Mona Lisa <had17007@byui.edu>
2019-03-12 17:06:29 -0600

    hail mary

--- quotes.js
+++ quotes.js
@@ -8,13 +8,14 @@ function buttonPress(){
 function grabAQuote(){
  removeQuote();
  let num = randomNumber();
+ let quote = "quote"+num;
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       let quotes = JSON.parse(this.responseText);
       quote = document.createElement("H1");
-      let text = document.createTextNode(quotes[num].text)
-      let author = document.createTextNode("\n-"+quotes[num].author);
+      let text = document.createTextNode(quotes.quote.text)
+      let author = document.createTextNode("\n-"+quotes.quote.author);
       quote.appendChild(text);
       quote.appendChild(author);
       document.getElementById("displayArea").appendChild(quote);
