5630780b5e659710a0bf2ee04ac0063eff958a0f
Mona Lisa <had17007@byui.edu>
2019-03-12 16:52:17 -0600

    format quote

--- quotes.js
+++ quotes.js
@@ -5,17 +5,26 @@ function buttonPress(){
 }
 
 function grabAQuote(){
-
+ removeQuote();
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       let quotes = JSON.parse(this.responseText);
       quote = document.createElement("H1");
-      text = document.createTextNode(quotes.quote1.text+quotes.quote1.author);
+      let text = document.createTextNode(quotes.quote1.text)
+      let author = document.createTextNode("-"+quotes.quote1.author);
       quote.appendChild(text);
+      quote.appendChild(author);
       document.getElementById("displayArea").appendChild(quote);
     }
   };  
   xhttp.open("GET", "quotes.json", true);
   xhttp.send();
+}
+
+function removeQuote() {
+  let quote = document.getElementById('displayArea');
+  while (quote.firstChild) {
+    quote.removeChild(quote.firstChild);  
+  }
 }
