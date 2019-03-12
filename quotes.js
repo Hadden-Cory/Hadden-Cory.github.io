b3b644b97f01a293b05d220bb89397a55d2363ed
Mona Lisa <had17007@byui.edu>
2019-03-12 17:17:06 -0600

    more random

--- quotes.js
+++ quotes.js
@@ -9,6 +9,7 @@ function grabAQuote(){
  removeQuote();
  let num = randomNumber();
  let quote = "quote"+num;
+ console.log(quote);
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -26,7 +27,9 @@ function grabAQuote(){
 }
 
 function randomNumber() {
-  var num = (Math.floor(Math.random() * 500) % 10);
+  let num = (Math.floor(Math.random() * 1000));
+  let num1 = (Math.floor(Math.random() * num) % 10);
+  return num1+1;
 }
 
 function removeQuote() {
