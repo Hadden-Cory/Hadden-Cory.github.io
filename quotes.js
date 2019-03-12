1b06cd166e131f24889f29087fba06422096073b
Mona Lisa <had17007@byui.edu>
2019-03-12 16:39:47 -0600

    ...

--- quotes.js
+++ quotes.js
@@ -1,11 +1,15 @@
+document.getElementById("quoteBtn").addEventListener("touch", buttonPress);
+
+function buttonPress(){
+  console.log("buttonPress");
+}
+
 function grabAQuote(){
 
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       let quotes = JSON.parse(this.responseText);
-      document.getElementById("thing").innerHTML = quotes.quote1.text;
-
       quote = document.createElement("H1");
       text = document.createTextNode(quotes.quote1.text+"\"<br>-"+ quotes.quote1.author);
       quote.appendChild(text);
@@ -15,32 +19,3 @@ function grabAQuote(){
   xhttp.open("GET", "quotes.json", true);
   xhttp.send();
 }
-
-
-
-
- 
-        //  var xhttp = new XMLHttpRequest();
-
-        //  xhttp.onreadystatechange = function() {
-        //   alert('here');
-         
-        //    if (this.readyState == 4 && this.status == 200) {
-
-        //      var item = JSON.parse(xhttp.response);
-
-        //     //  quote = document.createElement("H1");
-        //     //  text = document.createTextNode(item.quote+"\"<br>-"+ item.author);
-        //     //  quote.appendChild(text);
-         //    //  document.getElementById("thing").appendChild(quote);
-          
-        // printTime = document.createElement("H3");
-        // text = document.createTextNode(rowDAO.time);
-        // printTime.appendChild(text);
-        // document.getElementById("list").appendChild(printTime);
-
-        //      document.getElementById("list").innerHTML = "<li>\""+item.quote+"\"<br>-"+ item.author+"</li>";
-        //    };
-        
-        // xhttp.open("GET", "quotes.json", true);
-        // xhttp.send();
