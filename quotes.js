049b9f4103f5bbc2bcd339362288e1b01855027c
Mona Lisa <had17007@byui.edu>
2019-03-12 14:18:40 -0600

    Started wrok on quote wall

--- quotes.js
+++ quotes.js
@@ -1,17 +1,38 @@
 function grabAQuote(){
 
-        var xhttp = new XMLHttpRequest();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("thing").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("POST", "demo_post2.asp", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("fname=Henry&lname=Ford");
+}
 
-        xhttp.onreadystatechange = function() {
-            
-          if (this.readyState == 4 && this.status == 200) {
 
-            var item = JSON.parse(xhttp.response);
 
-            document.getElementById("list").innerHTML = "<li>\""+item.quote+"\"<br>-"+ item.author+"</li>";
-          };
-        };
+
+ 
+        //  var xhttp = new XMLHttpRequest();
+
+        //  xhttp.onreadystatechange = function() {
+        //   alert('here');
+         
+        //    if (this.readyState == 4 && this.status == 200) {
+
+        //      var item = JSON.parse(xhttp.response);
+
+        //     //  let tables = document.getElementById("list");
+        //     //  quote = document.createElement("LI");
+        //     //  text = document.createTextNode(item.quote+"\"<br>-"+ item.author);
+        //     //  quote.appendChild(text);
+          
+        //      document.getElementById("list").innerHTML = "<li>\""+item.quote+"\"<br>-"+ item.author+"</li>";
+        //    };
         
-       xhttp.open("GET", "quotes.json", true);
-       xhttp.send();
+        // xhttp.open("GET", "quotes.json", true);
+        // xhttp.send();
+}
 }
