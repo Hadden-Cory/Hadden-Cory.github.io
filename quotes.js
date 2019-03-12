5f97335d37f2ab103243650772941621dabad9f9
Mona Lisa <had17007@byui.edu>
2019-03-12 14:42:49 -0600

    Testing JSON Parse to screen

--- quotes.js
+++ quotes.js
@@ -3,7 +3,8 @@ function grabAQuote(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("thing").innerHTML = this.responseText;
+      let qutoes = JSON.parse(this.responseText);
+      document.getElementById("thing").innerHTML = quotes.quote1;
     }
   };  
   xhttp.open("GET", "quotes.json", true);
