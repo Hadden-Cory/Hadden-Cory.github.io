a30f5fdc68c07c84c868395b7b5fd3efea5fbfa8
Mona Lisa <had17007@byui.edu>
2019-04-06 12:13:57 -0600

    Added Interface options

--- dogGo.js
+++ dogGo.js
@@ -1,1 +1,28 @@
 //Our API: https://random.dog/woof.json
+
+
+class Dog{
+    constructor(name, date, price, description, bidCeiling){
+        this.name = name;
+        this.date = date;
+        this.price = price;
+        this.description = description;
+        this.highestBidder = "computer";
+        this.bidCeiling = bidCeiling;
+    }
+
+    bid(){
+        this.price = this.price + 5;
+        setHighestBidder();
+    }
+
+    setHighestBidder(){
+        if (this.price > this.bidCeiling){
+            this.highestBidder = "current User";
+        } else {
+            this.price = this.price + 5;
+            this.highestBidder = "computer";
+        }
+    }
+}
+
