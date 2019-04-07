9f915d63b5c76ea4b0c153b403c6599a24e9a044
Mona Lisa <had17007@byui.edu>
2019-04-06 21:32:15 -0600

    Building Dog Generator

--- dogGo.js
+++ dogGo.js
@@ -1,14 +1,15 @@
 //Our API: https://random.dog/woof.json'
 
 window.addEventListener('load', function () {
-    
+
     console.log('window loaded')
-    
+
     if (!location.hash) {
         console.log('unset location hash. Hash defaulted to #dogGo');
         location.hash = "#dogGo_List";
     }
 
+    randomDescription();
     setContent();
 
 }, false);
@@ -23,40 +24,40 @@ function setContent() {
 
     if (route == "#dogGo_Cart" || route == "#dogGo_Dog" || route == "#dogGo_List") {
 
-        console.log("Hash identified as"+location.hash+". Initiating AJAX")
-        displayContent(location.hash, function(content){
+        console.log("Hash identified as" + location.hash + ". Initiating AJAX")
+        displayContent(location.hash, function (content) {
             console.log("inserting new content");
             document.getElementById("content").innerHTML = content;
         });
 
-    }else {
+    } else {
         console.log("Hash unidentified. AJAX not called")
         document.getElementById("content").innerHTML = "Oops! Content Not Found";
     }
 }
 
-function displayContent(site, callback){
-    
+function displayContent(site, callback) {
+
     let xhttp = new XMLHttpRequest();
-    
-    xhttp.addEventListener("load", function(){
+
+    xhttp.addEventListener("load", function () {
         callback(xhttp.responseText);
     }, false);
 
     site = site.substr(1);
-    let ajaxUrl = site + '.html'; 
+    let ajaxUrl = site + '.html';
     console.log(ajaxUrl);
 
-        xhttp.open("GET", ajaxUrl);
-        xhttp.send(null);
-    }
+    xhttp.open("GET", ajaxUrl);
+    xhttp.send(null);
+}
 
 
 class Dog {
-    constructor(name, date, price, description, bidCeiling) {
+    constructor(name, description, bidCeiling) {
         this.name = name;
-        this.date = date;
-        this.price = price;
+        this.date = randomDate();
+        this.price = randomPrice();
         this.description = description;
         this.highestBidder = "computer";
         this.bidCeiling = bidCeiling;
@@ -76,3 +77,33 @@ class Dog {
         }
     }
 }
+
+function randomDate() {
+    var num = (Math.floor(Math.random() * 400) % 7) + 1;
+    let rndDate = new Date();
+    let newDate = rndDate.getDate() + num;
+    rndDate.setDate(newDate);
+    console.log('randomDate() returned ' + rndDate);
+    return rndDate;
+}
+
+function randomPrice() {
+    var num = ((Math.floor(Math.random() * 1400 % 10) * 100) + 300);
+    console.log(num);
+}
+
+function randomDescription() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            let descriptions = JSON.parse(this.responseText);
+        }};
+        xhttp.open("GET", "descriptions.json", true);
+        xhttp.send();
+
+        var num = (Math.floor(Math.random() * 400) % 7)
+
+        console.log(descriptions[num]);
+        return descriptions[num];
+    }
