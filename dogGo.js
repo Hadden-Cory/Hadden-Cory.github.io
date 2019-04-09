9c33518dbaac83d350bcf4faf71126b4124ed59f
Mona Lisa <had17007@byui.edu>
2019-04-08 22:18:45 -0600

    ...

--- dogGo.js
+++ dogGo.js
@@ -327,7 +327,7 @@ function buildList(index, value, array){
 let date = 'dog'+index+'date';
 let price = 'dog'+index+'price';
 let picture = 'dog'+index+'picture';
-
+console.log(date+", "+price+", "+picture)
 document.getElementById(date).innerHTML = value.date;
 document.getElementById(price).innerHTML = value.price;
 document.getElementById(picture).innerHTML = value.picture;   
