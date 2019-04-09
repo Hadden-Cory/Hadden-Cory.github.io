7217b6b141bf85febe93d8eb542bd935ed04c548
Mona Lisa <had17007@byui.edu>
2019-04-08 21:09:14 -0600

    ...

--- dogGo.js
+++ dogGo.js
@@ -9,52 +9,54 @@ window.namey = {
      * BSD licensed
      */
     // Lightweight JSONP fetcher - www.nonobtrusive.com
-    jsonP:(function(){
-        var a=0,c,f,b,d=this;
-        function e(j){
-            var i=document.createElement("script"),h=false;
-            i.src=j;
-            i.async=true;
-            i.onload=i.onreadystatechange=function(){
-                if(!h&&(!this.readyState||this.readyState==="loaded"||this.readyState==="complete")){
-                    h=true;i.onload=i.onreadystatechange=null;
-                    if(i&&i.parentNode){
+    jsonP: (function () {
+        var a = 0, c, f, b, d = this;
+        function e(j) {
+            var i = document.createElement("script"), h = false;
+            i.src = j;
+            i.async = true;
+            i.onload = i.onreadystatechange = function () {
+                if (!h && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
+                    h = true; i.onload = i.onreadystatechange = null;
+                    if (i && i.parentNode) {
                         i.parentNode.removeChild(i)
                     }
                 }
             };
-            if(!c){
-                c=document.getElementsByTagName("head")[0]
+            if (!c) {
+                c = document.getElementsByTagName("head")[0]
             }
             c.appendChild(i)
         }
-        
-        function g(h,j,k){
-            f="?";j=j||{};
-            for(b in j){
-                if(j.hasOwnProperty(b)){
-                    f+=encodeURIComponent(b)+"="+encodeURIComponent(j[b])+"&"
+
+        function g(h, j, k) {
+            f = "?"; j = j || {};
+            for (b in j) {
+                if (j.hasOwnProperty(b)) {
+                    f += encodeURIComponent(b) + "=" + encodeURIComponent(j[b]) + "&"
                 }
             }
-            var i="json"+(++a);
-            d[i]=function(l){k(l);
-                try{delete d[i]
+            var i = "json" + (++a);
+            d[i] = function (l) {
+                k(l);
+                try {
+                    delete d[i]
                 }
-                catch(m){
+                catch (m) {
 
                 }
-                d[i]=null;
+                d[i] = null;
             };
-            e(h+f+"callback="+i);
+            e(h + f + "callback=" + i);
             return i
         }
-        return{
-            get:g
+        return {
+            get: g
         }
     }
-    (
+        (
 
-    )
+        )
     ),
 
     /**
@@ -89,58 +91,58 @@ window.namey = {
      * passed in will be an array of names -- use them wisely.
      * 
      */
-    get : function(options) {
-	      var callback;
-	      var tmp_params = {};
+    get: function (options) {
+        var callback;
+        var tmp_params = {};
         var host = "namey.muffinlabs.com";
         //var host = window.location.host;
 
-	      if ( typeof(options) == "function" ) {
-	          callback = options;
-	      }
-	      else if ( typeof(options) == "object" ) {
-	          callback = options.callback;
+        if (typeof (options) == "function") {
+            callback = options;
+        }
+        else if (typeof (options) == "object") {
+            callback = options.callback;
 
-            if ( typeof(options.host) !== "undefined" ) {
+            if (typeof (options.host) !== "undefined") {
                 host = options.host;
             }
 
-	          if ( typeof(options.count) == "undefined" ) {
-		            options.count = 1;
-	          }
-	          tmp_params.count = options.count;
+            if (typeof (options.count) == "undefined") {
+                options.count = 1;
+            }
+            tmp_params.count = options.count;
 
-	          if ( typeof(options.type) != "undefined" && options.type != "both" ) {
-		            tmp_params.type = options.type;
-	          };
+            if (typeof (options.type) != "undefined" && options.type != "both") {
+                tmp_params.type = options.type;
+            };
 
-	          if ( options.type != "surname" && typeof(options.with_surname) != "undefined" ) {
-		            tmp_params.with_surname = options.with_surname;
-	          }
-	          if ( options.min_freq ) {
-		            tmp_params.min_freq = options.min_freq;
-		            tmp_params.max_freq = options.max_freq;
-	          }
-	          else if ( typeof(options.frequency) != "undefined" ) {
-		            tmp_params.frequency = options.frequency;
-	          }
+            if (options.type != "surname" && typeof (options.with_surname) != "undefined") {
+                tmp_params.with_surname = options.with_surname;
+            }
+            if (options.min_freq) {
+                tmp_params.min_freq = options.min_freq;
+                tmp_params.max_freq = options.max_freq;
+            }
+            else if (typeof (options.frequency) != "undefined") {
+                tmp_params.frequency = options.frequency;
+            }
 
-	      }
+        }
 
-	      this.jsonP.get('//' + host + '/name.json', tmp_params, function(d) {
-	          if ( typeof(callback) == "function" ) {
-		            callback(d);
-	          }
-	          else {
-		            console.log(d);
-	          }
-	      });
+        this.jsonP.get('//' + host + '/name.json', tmp_params, function (d) {
+            if (typeof (callback) == "function") {
+                callback(d);
+            }
+            else {
+                console.log(d);
+            }
+        });
     }
 }
 
 let descriptBank = [
-    "This one's a killer, great guard dog", 
-    "Great with kids, no shedding.", 
+    "This one's a killer, great guard dog",
+    "Great with kids, no shedding.",
     "Loves other dogs, very good compainion.",
     "Smart work dog, used to heard sheep.",
     "Originally owned by the queen.",
@@ -161,135 +163,136 @@ let descriptBank = [
     "Speaks fluent French and Italian."
 ]
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
 
-  console.log('window loaded')
+    console.log('window loaded')
 
-  if (!location.hash) {
-    console.log('unset location hash. Hash defaulted to #dogGo');
-    location.hash = "#dogGo_List";
-  }
+    if (!location.hash) {
+        console.log('unset location hash. Hash defaulted to #dogGo');
+        location.hash = "#dogGo_List";
+    }
 
-  setContent();
+    setContent();
 
-  let fido = new Dog();
-  console.log('creation');
-  console.log(fido);
-  initDog(fido, fetchName, fetchPicture);
-  console.log('final');
-  console.log(fido);
-setTimeout(() => {
-    console.log('finalfinal');
+    let fido = new Dog();
+    console.log('creation');
     console.log(fido);
-}, 5000);
+    initDog(fido, fetchName, fetchPicture);
+    console.log('final');
+    console.log(fido);
+    setTimeout(() => {
+        console.log('finalfinal');
+        console.log(fido);
+    }, 5000);
 }, false);
 
 window.addEventListener('hashchange', setContent, false);
 
 function setContent() {
 
-  console.log("HashChangeEvent Registared");
+    console.log("HashChangeEvent Registared");
 
-  let route = location.hash;
+    let route = location.hash;
 
-  if (route == "#dogGo_Cart" || route == "#dogGo_Dog" || route == "#dogGo_List") {
+    if (route == "#dogGo_Cart" || route == "#dogGo_Dog" || route == "#dogGo_List") {
 
-    console.log("Hash identified as " + location.hash + ". Initiating AJAX")
-    displayContent(location.hash, function(content) {
-      console.log("inserting new content");
-      document.getElementById("content").innerHTML = content;
-    });
+        console.log("Hash identified as " + location.hash + ". Initiating AJAX")
+        displayContent(location.hash, function (content) {
+            console.log("inserting new content");
+            document.getElementById("content").innerHTML = content;
+        });
 
-  } else {
-    console.log("Hash unidentified. AJAX not called")
-    document.getElementById("content").innerHTML = "Oops! Content Not Found";
-  }
+    } else {
+        console.log("Hash unidentified. AJAX not called")
+        document.getElementById("content").innerHTML = "Oops! Content Not Found";
+    }
 }
 
 function displayContent(site, callback) {
 
-  let xhttp = new XMLHttpRequest();
+    let xhttp = new XMLHttpRequest();
 
-  xhttp.addEventListener("load", function() {
-    callback(xhttp.responseText);
-  }, false);
+    xhttp.addEventListener("load", function () {
+        callback(xhttp.responseText);
+    }, false);
 
-  site = site.substr(1);
-  let ajaxUrl = site + '.html';
-  console.log(ajaxUrl);
+    site = site.substr(1);
+    let ajaxUrl = site + '.html';
+    console.log(ajaxUrl);
 
-  xhttp.open("GET", ajaxUrl);
-  xhttp.send(null);
+    xhttp.open("GET", ajaxUrl);
+    xhttp.send(null);
 }
 
 class Dog {
-  constructor() {
-    this.name = "unnamed";
-    this.date = randomDate();
-    this.price = randomPrice();
-    this.description = randomDescription();
-    this.highestBidder = "computer";
-    this.bidCeiling = (Math.floor(Math.random() * 1000 % 10) * 10) + this.price;
-    this.picture = "http://pre02.deviantart.net/4d35/th/pre/f/2007/280/8/1/droopy__s_head_by_drawright.png";
-}
-
-  setName(name){
-    this.name = name; 
-  }
-
-  setPicture(url){
-      this.picture = url
-      console.log('picute after init():');
-      console.log(this);
-  }
-
-  bid() {
-    this.price = this.price + 5;
-    setHighestBidder();
-  }
-
-  setHighestBidder() {
-    if (this.price > this.bidCeiling) {
-      this.highestBidder = "current User";
-    } else {
-      this.price = this.price + 5;
-      this.highestBidder = "computer";
+    constructor() {
+        this.name = "unnamed";
+        this.date = randomDate();
+        this.price = randomPrice();
+        this.description = randomDescription();
+        this.highestBidder = "computer";
+        this.bidCeiling = (Math.floor(Math.random() * 1000 % 10) * 10) + this.price;
+        this.picture = "http://pre02.deviantart.net/4d35/th/pre/f/2007/280/8/1/droopy__s_head_by_drawright.png";
     }
-  }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    setPicture(url) {
+        this.picture = url;
+        console.log('picute after init():');
+        console.log(this);
+    }
+
+    bid() {
+        this.price = this.price + 5;
+        setHighestBidder();
+    }
+
+    setHighestBidder() {
+        if (this.price > this.bidCeiling) {
+            this.highestBidder = "current User";
+        } else {
+            this.price = this.price + 5;
+            this.highestBidder = "computer";
+        }
+    }
 }
 
-async function fetchName(dog){
-    namey.get(function(n) {
-        
+async function fetchName(dog) {
+    namey.get(function (n) {
+
         dog.setName(n[0])
         console.log("in fetchname");
         console.log(dog);
 
-});
+    });
 }
 
-function fetchPicture(dog){
-const url = 'https://random.dog/woof.json';
-  let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+function fetchPicture(dog) {
+    const url = 'https://random.dog/woof.json';
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
 
-    if (this.readyState == 4 && this.status == 200) {
-        let pictureObj = JSON.parse(this.responseText);
-        let picture = pictureObj[0];
-        dog.setPicture(picture);
-    }
-  };
+        if (this.readyState == 4 && this.status == 200) {
+            let pictureObj = JSON.parse(this.responseText);
+            let picture = pictureObj[0];
+            console.log(picture);
+            dog.setPicture(picture);
+        }
+    };
 
-  xhttp.open("GET", url, true);
-  xhttp.send();
+    xhttp.open("GET", url, true);
+    xhttp.send();
 }
 
-function initDog(dog , callback0, callback1){
-   //fetchName(dog.setname());
-   // fetchPicture(dog.setPicture(picture));
-   dog.setName('james');
-   callback0(dog);
-   callback1(dog);
+function initDog(dog, callback0, callback1) {
+    //fetchName(dog.setname());
+    // fetchPicture(dog.setPicture(picture));
+    dog.setName('james');
+    callback0(dog);
+    callback1(dog);
     console.log('after init():');
     console.log(dog);
 }
@@ -297,36 +300,36 @@ function initDog(dog , callback0, callback1){
 
 
 function randomDate() {
-  var num = (Math.floor(Math.random() * 400) % 7) + 1;
-  let rndDate = new Date();
-  let newDate = rndDate.getDate() + num;
-  rndDate.setDate(newDate);
-  return rndDate;
+    var num = (Math.floor(Math.random() * 400) % 7) + 1;
+    let rndDate = new Date();
+    let newDate = rndDate.getDate() + num;
+    rndDate.setDate(newDate);
+    return rndDate;
 }
 
 function randomPrice() {
-  var num = ((Math.floor(Math.random() * 1400 % 10) * 100) + 100);
-  return num;
+    var num = ((Math.floor(Math.random() * 1400 % 10) * 100) + 100);
+    return num;
 }
 
 function randomDescription() {
-    
-    var num = ((Math.floor(Math.random() * 400) % 20)+1)
+
+    var num = ((Math.floor(Math.random() * 400) % 20) + 1)
     return descriptBank[num];
 
-//   let xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
+    //   let xhttp = new XMLHttpRequest();
+    //   xhttp.onreadystatechange = function() {
 
-//     if (this.readyState == 4 && this.status == 200) {
-//         let descriptions = [];
-//         descriptions = JSON.parse(this.responseText);
-//       var num = ((Math.floor(Math.random() * 400) % 20)+1);
-//       num = "description" + num;
-//       console.log('randomDescription() returned '+ descriptions[num]);
-//       return descriptions[num];
-//     }
-//   };
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         let descriptions = [];
+    //         descriptions = JSON.parse(this.responseText);
+    //       var num = ((Math.floor(Math.random() * 400) % 20)+1);
+    //       num = "description" + num;
+    //       console.log('randomDescription() returned '+ descriptions[num]);
+    //       return descriptions[num];
+    //     }
+    //   };
 
-//   xhttp.open("GET", "descriptions.json", true);
-//   xhttp.send();
+    //   xhttp.open("GET", "descriptions.json", true);
+    //   xhttp.send();
 }
